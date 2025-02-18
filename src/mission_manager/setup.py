996d3d0fc9cc86796aeb098287da25c07f86d03e
Pride-Alcott <s229701698@mandela.ac.za>
2025-02-18 15:25:49 +0200

    Update setup.py

--- src/mission_manager/setup.py
+++ src/mission_manager/setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='pride',
-    maintainer_email='pride@todo.todo',
+    maintainer_email='pride@mandela.ac.za',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
